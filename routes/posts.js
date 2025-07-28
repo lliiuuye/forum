113300d04f90669d169c3e99f2de64288ba240c6
Bing Liu <lliiuuye@gmail1.com>
2025-07-28 18:14:09 -0400

    change post, add new post

--- routes/posts.js
+++ routes/posts.js
@@ -101,6 +101,13 @@ router.post("/:id/delete", async (req, res) => {
   res.status(403).send("你没有权限删除这个帖子");
 });
 
+// 发帖页面（显示发帖表单）
+router.get("/new", (req, res) => {
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
+  res.render("new-post", { user: req.session.user });
+});
 
 
 
