180d293f603caa6c6c4b62eeeaab8f7c80869ca1
Bing Liu <lliiuuye@gmail1.com>
2025-07-28 22:50:09 -0400

    change db.js keep secret of database info

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config(); // 加载 .env 配置
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "forum_db",
-  charset: 'utf8mb4'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  charset: process.env.DB_CHARSET || 'utf8mb4'
 });
 
 pool.getConnection().then((connection) => {
   connection.query('SET NAMES utf8mb4');
-  connection.release(); // 释放连接
+  connection.release();
 });
 
 module.exports = pool;
-
